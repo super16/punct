95b0cd5b85610632c0f596e9fd01db1e18f01c62
Mark <super16mail@gmail.com>
2022-12-29 01:28:27 +0300

    Refactor from vue to petite-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
     'airbnb-typescript/base',
     'eslint:recommended',
     'plugin:import/recommended',
-    'plugin:vue/vue3-recommended',
   ],
-  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    extraFileExtensions: ['.vue'],
     parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
     sourceType: 'module',
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,38 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'petite-vue';
 import './index.css';
+
+interface Application {
+  text: string,
+  processedText: string,
+  repoLink: string,
+  show: boolean,
+  processText: Function,
+  clearText: Function,
+}
+
+const App: Application = {
+  text: 'Вставьте сюда текст и нажмите на кнопку «Показать», ' +
+  'чтобы от текста остались только знаки препинания.',
+  processedText: '',
+  repoLink: 'https://github.com/super16',
+  show: false,
+  processText(): void {
+    const punctuation: RegExpMatchArray | null =
+      this.text.match(/\p{P}/gu);
+    if (punctuation) {
+      this.processedText = punctuation.join('&#8201;');
+      this.processedText = this.processedText.replaceAll(
+        /(\r\n|\n|\r)/gu,
+        '',
+      );
+    }
+    this.show = true;
+  },
+  clearText(): void {
+    this.text = '';
+    this.processedText = '';
+    this.show = false;
+  },
+};
 
 createApp(App).mount('#app');
